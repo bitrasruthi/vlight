7f5fd704a1df575a6940354d5069621ecaefb783
SaiRamaKrishna <sairamakrishna2@gmail.com>
2021-12-01 15:53:52 +0530

    conflict

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
+import React from "react";
+import Admin from "./layouts/Admin";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Login from "./components/Logins/Login";
+import { Provider } from "react-redux";
 import store from "./reduxstore/store";
+import ELogin from "./components/Logins/eLogin";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Settings from './components/Common/settings';
+import Sidebar from "./components/Sidebar/Sidebar";
+import Authy from "layouts/Auth";
+import AddNew from "./components/Admin Files/addNewEmp";
+import NavBar from "./components/Common/navbar";
+import Logout from "./components/Logins/logout";
+import EmpList from "./components/Admin Files/emplist";
+import Dashboard from "components/Common/dashboard";
+import Footer from "components/Footers/AdminFooter";
+import EDashboard from "./components/Common/edashboard";
+import TimeCard from "components/Employee Files/timecard";
+import AttList from "./components/Employee Files/myattendance";
+import LeaveList from "./components/Admin Files/leavelist";
+import LeaveForm from "./components/Employee Files/leaveform";
+import ELeaveList from "./components/Employee Files/eleavelist";
+import ApproveReject from "./components/Admin Files/approvereject";
+import Register from "./components/Logins/register";
+import Countdown from "./components/Logins/countdown";
+import EmpAttList from "components/Admin Files/empAttList";
 
 class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          <React.Fragment>
             <ToastContainer />
 
             <main className="container">
-            <Route path="/settings" component={Settings} /> 
+              <Switch>
+                {/* <Route path="/notready" component={NotFound} /> */}
+                <Route path="/elogin" component={ELogin} />
+                <Route path="/register" component={Register} />
+                <Route path="/addnewemp" component={AddNew} />
+                <Route path="/emplist" component={EmpList} />
+                <Route path="/leavelist" component={LeaveList} />
+                <Route path="/logout" component={Logout} />
 
                 <Route path="/navbar" component={NavBar} />
                 <Route path="/dashboard" component={Dashboard} />
@@ -20,7 +55,20 @@ class App extends React.Component {
                 <Route path="/leaveapproval/:id" component={ApproveReject} />
                 <Route path="/empattlist/:id" component={EmpAttList} />
 
+                <Route path="/" component={Login} />
+                <Redirect from="/" exact to="/home" />
                 <Redirect to="/not-found" />
 
+                {/* <Route path="/admin" render={(props) => <AdminLayout {...props} />} /> */}
+                {/* <Route path="/auth" render={(props) => <AuthLayout {...props} />} /> */}
+              </Switch>
+              <Footer />
             </main>
+          </React.Fragment>
+        </Provider>
+      </div>
+    );
+  }
 }
+
+export default App;
